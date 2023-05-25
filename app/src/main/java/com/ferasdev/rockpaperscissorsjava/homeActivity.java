c82697ae4b9193cf8efbfa8735b4e113da6be6ef
mogh7 <mohammed.adel10102001@gmail.com>
2023-05-25 19:38:09 +0300

    add home page

---
+++ app/src/main/java/com/ferasdev/rockpaperscissorsjava/homeActivity.java
@@ -0,0 +1,21 @@
+package com.ferasdev.rockpaperscissorsjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activty);
+    }
+
+    public void createGame(View view) {
+    }
+
+    public void joinGame(View view) {
+    }
+}
